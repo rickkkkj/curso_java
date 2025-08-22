282dd2828ca87ced022870bf7d7c9c9a50d1587b
Ricardo Martins <ricardomswork@gmail.com>
2025-08-22 14:55:22 -0300

    Aulas sobre enuns.

---
+++ secoes/s12/aulas/entities/Order.java
@@ -0,0 +1,42 @@
+package s12.aulas.entities;
+
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+
+public class Order {
+    private Integer id;
+    private Date moment;
+    private OrderStatus status;
+
+    public Order() {
+    }
+
+    public Order( Integer id, Date moment, OrderStatus status) {
+        this.id = id;
+        this.moment = moment;
+        this.status = status;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+    public Date getMoment() {
+        return moment;
+    }
+    public OrderStatus getStatus() {
+        return status;
+    }
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    public void setMoment(Date moment) {
+        this.moment = moment;
+    }
+    public void setStatus(OrderStatus status) {
+        this.status = status;
+    }
+
+    public String toString(){
+        return "Id: " + getId() + "\nDate: " + getMoment() + "\nStatus: " + getStatus();
+    }
+}
---
+++ secoes/s12/aulas/entities/OrderStatus.java
@@ -0,0 +1,8 @@
+package s12.aulas.entities;
+
+public enum OrderStatus{
+    PENDING_PAYMENT,
+    PROCESSING,
+    SHIPPED,
+    DELIVERED;
+}
